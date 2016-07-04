5a40a2405426b4fe606d1fde4473e66f81e77125
vietlong2110 <vietlong2110@gmail.com>
2016-07-04 13:34:39 +0700

    test server

--- rabbitAPI/autoupdate.js
+++ rabbitAPI/autoupdate.js
@@ -14,20 +14,53 @@ var j = 0, cache = [];
 
 async.forever(function(callback) {
 	var articles = [];
-	
-	async.each(feedList, function(feed, cb) {
-		console.log('Start loading ' + feed);
-		RSS.feedParse(feed, function(links) {
-			console.log('End loading ' + feed);
-			cache = cache.concat(links);
-			cb();
+
+	if (cache.length > 0) {
+		async.each(feedList, function(feed, cb) {
+			console.log('Start loading ' + feed);
+			RSS.feedParse(feed, function(links) {
+				console.log('End loading ' + feed);
+				cache = cache.concat(links);
+				cb();
+			});
+		}, function(err) {
+			if (err) {
+				console.log(err);
+				callback(cache);
+			}
+			console.log(cache.length);
+			var maxComingArticle = cache.length;
+			var i = 0;
+
+			async.whilst(function() { return i < maxComingArticle; },
+			function(cb) {
+				var Extract = require('./serverController/extract.js');
+				console.log('Start extracting ' + cache[0].link);
+				Extract.extractImage(cache[0].link, function(thumbnail) {
+					console.log('End extracting ' + cache[0].link);
+					articles.push({
+						url: cache[0].link,
+						title: entities.decode(cache[0].title),
+						thumbnail: thumbnail,
+						publishedDate: cache[0].publishedDate
+					});
+					cache.shift();
+					i++;
+					cb();
+				});
+			}, function() {
+				console.log(cache.length);
+				// var Save = require('../serverController/save.js');
+
+				// Save.saveArticle(articles, function() {
+				// 	console.log('All news articles are saved!');
+					j++;
+					callback();
+				// });
+			});
 		});
-	}, function(err) {
-		if (err) {
-			console.log(err);
-			callback(cache);
-		}
-		console.log(cache.length);
+	}
+	else {
 		var maxComingArticle = cache.length;
 		var i = 0;
 
@@ -57,5 +90,5 @@ async.forever(function(callback) {
 				callback();
 			// });
 		});
-	});
+	}
 });

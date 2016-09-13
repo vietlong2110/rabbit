fc1d84a30f69e206e74d16f7613be3873dcc58e6
vietlong2110 <vietlong2110@gmail.com>
2016-09-13 03:28:20 -0400

    update the new way to update

--- rabbitAPI/updatemedia.js
+++ rabbitAPI/updatemedia.js
@@ -9,97 +9,109 @@ var RSS = require('./serverController/rss.js');
 var Article = require('./models/articles.js');
 var Extract = require('./serverController/extract.js');
 var Save = require('./serverController/save.js');
+var feed_link = require('./seed/feed_link.js');
+var feedList = feed_link.ninegag;
 var cache = [], instaCache = [], saved = true;
 	// instagram: function(callback) {
-			// 	var FollowKeyword = require('./models/followkeywords.js');
+	// 			var FollowKeyword = require('./models/followkeywords.js');
 
-			// 	if (instaCache.length === 0) {
-			// 		FollowKeyword.find({}).exec(function(err, queries) {
-			// 			if (err) {
-			// 				console.log(err);
-			// 				callback();
-			// 			}
-			// 			else {
-			// 				instaCache = queries;
-			// 				var limitQueries = Math.min(instaCache.length, 8);
-			// 				var queryList = instaCache.slice(0, limitQueries);
+	// 			if (instaCache.length === 0) {
+	// 				FollowKeyword.find({}).exec(function(err, queries) {
+	// 					if (err) {
+	// 						console.log(err);
+	// 						callback();
+	// 					}
+	// 					else {
+	// 						instaCache = queries;
+	// 						var limitQueries = Math.min(instaCache.length, 8);
+	// 						var queryList = instaCache.slice(0, limitQueries);
 
-			// 				async.each(queryList, function(keyword, cb) {
-			// 					var Feed = require('./clientController/feed.js');
+	// 						async.each(queryList, function(keyword, cb) {
+	// 							var Feed = require('./clientController/feed.js');
 
-			// 					Feed.searchInstaFeed(keyword.query, function(mediaResult) {
-			// 						media = media.concat(mediaResult);
-			// 						instaCache.shift();
-			// 						cb();
-			// 					});
-			// 				}, function(err) {
-			// 					if (err)
-			// 						console.log(err);
+	// 							Feed.searchInstaFeed(keyword.query, function(mediaResult) {
+	// 								media = media.concat(mediaResult);
+	// 								instaCache.shift();
+	// 								cb();
+	// 							});
+	// 						}, function(err) {
+	// 							if (err)
+	// 								console.log(err);
 
-			// 					callback();
-			// 				});
-			// 			}
-			// 		});
-			// 	}
-			// 	else {
-			// 		var limitQueries = Math.min(instaCache.length, 8);
-			// 		var queryList = instaCache.slice(0, limitQueries);
+	// 							callback();
+	// 						});
+	// 					}
+	// 				});
+	// 			}
+	// 			else {
+	// 				var limitQueries = Math.min(instaCache.length, 8);
+	// 				var queryList = instaCache.slice(0, limitQueries);
 
-			// 		async.each(queryList, function(keyword, cb) {
-			// 			var Feed = require('./clientController/feed.js');
+	// 				async.each(queryList, function(keyword, cb) {
+	// 					var Feed = require('./clientController/feed.js');
 
-			// 			Feed.searchInstaFeed(keyword.query, function(mediaResult) {
-			// 				media = media.concat(mediaResult);
-			// 				instaCache.shift();
-			// 				cb();
-			// 			});
-			// 		}, function(err) {
-			// 			if (err)
-			// 				console.log(err);
+	// 					Feed.searchInstaFeed(keyword.query, function(mediaResult) {
+	// 						media = media.concat(mediaResult);
+	// 						instaCache.shift();
+	// 						cb();
+	// 					});
+	// 				}, function(err) {
+	// 					if (err)
+	// 						console.log(err);
 
-			// 			callback();
-			// 		});
-			// 	}
+	// 					callback();
+	// 				});
+	// 			}
 	// }
 	
 setInterval(function() {
 	if (saved) {
 		saved = false;
 		var media = [];
-		var feed_link = require('./seed/feed_link.js');
-		var feedList = feed_link.ninegag;
 
-		async.each(feedList, function(feed, cb1) {
-			console.log('Start loading ' + feed);
-			RSS.feedParse(feed, function(links) {
-				console.log('End loading ' + feed);
-				cache = cache.concat(links);
-				cb1();
-			});
-		}, function(err) {
-			if (err) {
-				console.log(err);
-				// callback(cache);
-			}
-			console.log(cache.length);
-			var maxComingArticle = cache.length;
-			var i = 0;
-
-			async.whilst(function() { return i < maxComingArticle; },
+		async.waterfall([
 			function(cb) {
-				Article.findOne({title: entities.decode(cache[0].title)}).exec(function(err, article) {
-					if (article === null) {
-						var icon = require('./seed/icon_link.js');
-						var avatar = icon.ninegag;
-						console.log('Start extracting ' + cache[0].link);
-						Extract.extractImage(cache[0].link, function(image) {
-							if (image === null) {
+				if (cache.length === 0) {
+					async.each(feedList, function(feed, cb1) {
+						console.log('Start loading ' + feed);
+						RSS.feedParse(feed, function(links) {
+							console.log('End loading ' + feed);
+							cache = cache.concat(links);
+							cb1();
+						});
+					}, function(err) {
+						if (err) {
+							console.log(err);
+							return cb(err);
+						}
+						cb(null);
+					});
+				}
+				else cb(null);
+			},
+			function(cb) {
+				console.log(cache.length);
+				var maxComingArticle = cache.length;
+				var i = 0;
+
+				async.whilst(function() { return i < maxComingArticle; },
+				function(cb2) {
+					Article.findOne({title: entities.decode(cache[0].title)})
+					.exec(function(err, article) {
+						if (article === null) {
+							var icon = require('./seed/icon_link.js');
+							var avatar = icon.ninegag;
+							console.log('Start extracting ' + cache[0].link);
+							var regex = /<img[^>]+src\s*=[^>]*['"](http[^'"]+)[^>]*['"][^>]*>/g;
+							var images = regex.exec(cache[0].content);
+							if (images === null) {
 								console.log('NULL DETECTED!');
 								cache.shift();
 								i++;
-								cb();
+								cb2();
 							}
 							else {
+								image = images[1];
 								if (image.substr(image.length-4, 4) === ".gif")
 									image = image.replace("a.gif", ".jpg");
 								Extract.extractKeyword(null, entities.decode(cache[0].title),
@@ -119,26 +131,27 @@ setInterval(function() {
 									});
 									cache.shift();
 									i++;
-									cb();
+									cb2();
 								});
 							}
-						});
-					}
-					else {
-						console.log('This article was saved');
-						cache.shift();
-						i++;
+						}
+						else {
+							console.log('This article was saved');
+							cache.shift();
+							i++;
+							cb2();
+						}
+					});
+				}, function() {
+					console.log(cache.length);
+					Save.saveMediaArticle(media, function() {
+						saved = true;
+						console.log('All news articles are saved!');
 						cb();
-					}
+					});
 				});
-			}, function() {
-				console.log(cache.length);
-				Save.saveMediaArticle(media, function() {
-					saved = true;
-					console.log('All news articles are saved!');
-				});
-			});
-		});
+			}
+		]);
 	}
 	else console.log(cache.length);
 }, 60 * 1000);
--- rabbitAPI/updatenews.js
+++ rabbitAPI/updatenews.js
@@ -18,63 +18,74 @@ setInterval(function() {
 		saved = false;
 		var articles = [];
 
-		async.each(feedList, function(feed, cb) {
-			console.log('Start loading ' + feed);
-			RSS.feedParse(feed, function(links) {
-				console.log('End loading ' + feed);
-				cache = cache.concat(links);
-				cb();
-			});
-		}, function(err) {
-			if (err) {
-				console.log(err);
-				// callback(cache);
-			}
-			console.log(cache.length);
-			var maxComingArticle = cache.length;
-			var i = 0;
-
-			async.whilst(function() { return i < maxComingArticle; },
+		async.waterfall([
 			function(cb) {
-				Article.findOne({title: entities.decode(cache[0].title)}).exec(function(err, article) {
-					if (article === null) {
-						console.log('Start extracting ' + cache[0].link);
-						Extract.extractImage(cache[0].link, function(thumbnail) {
-							Extract.extractContent(entities.decode(cache[0].title), cache[0].link,
-							function(originKeywordSet, keywordSet, tf, titleKeywordSet, tfTitle) {
-								console.log('End extracting ' + cache[0].link);
-								articles.push({
-									url: cache[0].link,
-									title: entities.decode(cache[0].title),
-									thumbnail: thumbnail,
-									publishedDate: cache[0].publishedDate,
-									titleKeywords: titleKeywordSet,
-									tfTitle: tfTitle,
-									keywords: keywordSet,
-									originkeywords: originKeywordSet,
-									tf: tf
-								});
-								cache.shift();
-								i++;
-								cb();
-							});
+				if (cache.length === 0) {
+					async.each(feedList, function(feed, cb1) {
+						console.log('Start loading ' + feed);
+						RSS.feedParse(feed, function(links) {
+							console.log('End loading ' + feed);
+							cache = cache.concat(links);
+							cb1();
 						});
-					}
-					else {
-						console.log('This article was saved');
-						cache.shift();
-						i++;
-						cb();
-					}
-				});
-			}, function() {
+					}, function(err) {
+						if (err) {
+							console.log(err);
+							return cb(err);
+						}
+						cb(null);
+					});
+				}
+				else cb(null);
+			},
+			function(cb) {
 				console.log(cache.length);
-				Save.saveArticle(articles, function() {
-					saved = true;
-					console.log('All news articles are saved!');
+				var maxComingArticle = Math.min(50, cache.length);
+				var i = 0;
+
+				async.whilst(function() { return i < maxComingArticle; },
+				function(cb2) {
+					Article.findOne({title: entities.decode(cache[0].title)}).exec(function(err, article) {
+						if (article === null) {
+							console.log('Start extracting ' + cache[0].link);
+							Extract.extractImage(cache[0].link, function(thumbnail) {
+								Extract.extractContent(entities.decode(cache[0].title), cache[0].link,
+								function(originKeywordSet, keywordSet, tf, titleKeywordSet, tfTitle) {
+									console.log('End extracting ' + cache[0].link);
+									articles.push({
+										url: cache[0].link,
+										title: entities.decode(cache[0].title),
+										thumbnail: thumbnail,
+										publishedDate: cache[0].publishedDate,
+										titleKeywords: titleKeywordSet,
+										tfTitle: tfTitle,
+										keywords: keywordSet,
+										originkeywords: originKeywordSet,
+										tf: tf
+									});
+									cache.shift();
+									i++;
+									cb2();
+								});
+							});
+						}
+						else {
+							console.log('This article was saved');
+							cache.shift();
+							i++;
+							cb2();
+						}
+					});
+				}, function() {
+					console.log(cache.length);
+					Save.saveArticle(articles, function() {
+						saved = true;
+						console.log('All news articles are saved!');
+						cb();
+					});
 				});
-			});
-		});
+			}
+		]);
 	}
 	else console.log(cache.length);
 }, 60 * 1000);

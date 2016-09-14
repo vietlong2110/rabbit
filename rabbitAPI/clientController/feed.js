6a1537d100c0b660bc3e96db2addbedd192d5694
vietlong2110 <vietlong2110@gmail.com>
2016-09-14 00:35:50 -0400

    debug

--- rabbitAPI/clientController/feed.js
+++ rabbitAPI/clientController/feed.js
@@ -20,14 +20,14 @@ var searchFeed = function(q, callback) {
 
 	var searchResult = [];
 	searchFuncs.Search(query, function(articles, evalScore) {
-		console.log(articles.length);
-		console.log(evalScore.length);
+		// console.log(articles.length);
+		// console.log(evalScore.length);
 		for (i = 0; i < articles.length; i++) {
 			var todayArr = [];
 			todayArr.push(articles[i].publishedDate.getDate());
 			todayArr.push(articles[i].publishedDate.getMonth());
 			todayArr.push(articles[i].publishedDate.getFullYear());
-			
+			console.log('In here!');
 			searchResult.push({
 				evalScore: evalScore[i],
 				today: todayArr,
@@ -40,6 +40,7 @@ var searchFeed = function(q, callback) {
 				publishedDate: articles[i].publishedDate,
 				media: articles[i].media
 			});
+			console.log('In here 2!');
 		}
 		console.log(searchResult);
 		callback(searchResult);

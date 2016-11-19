6bfe5c4d446801a622d61ee60762db9f4da3f4a2
Long Le <vietlong2110@gmail.com>
2016-11-19 17:05:56 -0500

    test search suggestion

--- rabbitAPI/computation.js
+++ rabbitAPI/computation.js
@@ -1,45 +1,41 @@
 console.log('Computation is running!');
 
-var async = require('async');
 var database = require('./database.js');
 var myCache = require('./models/caches.js');
+var saved = true;
 
-async.forever(function(callback) {
-	var Compute = require('./serverController/compute.js');
-						
-	Compute.computeKeywordsWeight(function(keywords) {
-		console.log('Evaluated weight of all keywords!');
+// setInterval(function() {
+	if (saved) {
+		saved = false;
+		var Compute = require('./serverController/compute.js');
+							
+		Compute.computeKeywordsWeight(function(keywords) {
+			console.log('Evaluated weight of all keywords!');
 
-		var query = {key: 'keywordArray'};
-		var update = {$set: {value: keywords}};
-		var options = {upsert: true};
-		myCache.findOneAndUpdate(query, update, options).exec(function(err, doc) {
-			if (err) {
-				console.log(err);
-				callback();
-			}
-			else {
-				var Algo = require('./libs/classic-algorithm.js');
-				var segmentTree = Algo.initializeSegmentTree(keywords);
-				var query = {key: 'keywordTree'};
-				var update = {$set: {value: segmentTree}};
-				var options = {upsert: true};
+			var query = {key: 'keywordArray'};
+			var update = {$set: {value: keywords}};
+			var options = {upsert: true};
+			myCache.findOneAndUpdate(query, update, options).exec(function(err, doc) {
+				if (err) {
+					console.log(err);
+					callback();
+				}
+				else {
+					var Algo = require('./libs/classic-algorithm.js');
+					var segmentTree = Algo.initializeSegmentTree(keywords);
+					var query = {key: 'keywordTree'};
+					var update = {$set: {value: segmentTree}};
+					var options = {upsert: true};
 
-				myCache.findOneAndUpdate(query, update, options).exec(function(err, doc) {
-					if (err) {
-						console.log(err);
-						callback();
-					}
-					else {
-						console.log('Tree is set!');
-						setTimeout(function() {
-							callback();
-						}, 1000 * 60 * 15);
-					}
-				});
-			}
+					myCache.findOneAndUpdate(query, update, options).exec(function(err, doc) {
+						if (err)
+							console.log(err);
+						saved = true;
+					});
+				}
+			});
 		});
-	});
-});
+	}
+// }, 60 * 20 * 1000);
 
 // setInterval(compute, 1000 * 60 * 15);
